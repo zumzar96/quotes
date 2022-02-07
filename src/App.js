cf258be49d9529db49a8edde5ed0931f00ad4232
ismail <ismailgim96@gmail.com>
2022-02-07 09:42:36 +0100

    Improving nested routes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import Layout from './components/layout/Layout';
 
 function App() {
     return (
-        <Layout>it p
-
+        <Layout>
             <Switch>
                 <Route path='/' exact>
                     <Redirect to='/quotes' />
